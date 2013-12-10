c5048860516244a45ce5e34015346b39a5dfe5b1
Mira <m13hall@gmail.com>
2013-12-10 10:51:49 -0600

    clean up

--- src/hallmira/JSONObject.java
+++ src/hallmira/JSONObject.java
@@ -2,40 +2,54 @@ package hallmira;
 
 import java.util.ArrayList;
 import hallmira.JSONParser;
+
 public class JSONObject {
-	
-    	//Fields
-    	ArrayList<JSONPair> pairs;
-    	
-    	//Constructors
-	public JSONObject(String name, Object value) {
-	    this.pairs.add(new JSONPair(name, value));
-	}
-	public JSONObject(ArrayList<JSONPair> pairs) {
-	    this.pairs = pairs;
-	}
-	
-	public JSONObject(JSONParser parser) {
-	    this.pairs = new ArrayList<JSONPair>();
-	}
-	
-	//methods
-	public void addPair(JSONParser parser) {
-	    this.pairs.add(new JSONPair(parser));
-	}
-   
-   public class JSONPair {
+
+    // Fields
+    ArrayList<JSONPair> pairs;
+
+    // Constructors
+    public JSONObject(String name, Object value) {
+	this.pairs.add(new JSONPair(name, value));
+    }
+
+    public JSONObject(ArrayList<JSONPair> pairs) {
+	this.pairs = pairs;
+    }
+
+    public JSONObject(JSONParser parser) {
+	this.pairs = new ArrayList<JSONPair>();
+    }
+
+    // methods
+    public void addPair(JSONParser parser) {
+	this.pairs.add(new JSONPair(parser));
+    }
+
+    public class JSONPair {
+	// Fields
 	String name;
 	Object value;
 
+	// constructors
 	public JSONPair(String name, Object value) {
 	    this.name = name;
 	    this.value = value;
 	}
+
 	public JSONPair(JSONParser parser) {
 	    this.name = (String) parser.parse();
-	    parser.current++; //skip over the ":"
-	    this.value = parser.parse(); //change to look for the right 
+	    parser.current++; // skip over the ":"
+	    this.value = parser.parse(); // change to look for the right
 	}
-   }//JSONPair
-} //JSONObject
+	
+	// methods
+	public String name() {
+	    return this.name;
+	}
+	
+	public Object value() {
+	    return this.value;
+	}
+    }// JSONPair
+} // JSONObject
